1618759659bc4a62c78a04735d25dd29342f2e64
250542024 <250542024@firat.edu.tr>
2025-10-23 14:55:06 +0300

    saryajava

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,11 @@
+public class AsciiName {
+    public static void main(String[] args) {
+        System.out.println("  _____   _    _  _____  __     __   _ ");
+        System.out.println(" / ____| | |  | ||  __ \\ \\ \\   / /  | |");
+        System.out.println("| (___   | |__| || |__) | \\ \\_/ / __| |");
+        System.out.println(" \\___ \\  |  __  ||  _  /   \\   / / _` |");
+        System.out.println(" ____) | | |  | || | \\ \\    | | | (_| |");
+        System.out.println("|_____/  |_|  |_||_|  \\_\\   |_|  \\__,_|");
+        System.out.println("             S A R Y A               ");
+    }
+}
